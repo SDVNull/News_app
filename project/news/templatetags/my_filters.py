5109f2dbbfd00267f6b590b3ba7735c108d404b8
Dmitriy Safronov <dimasafronov55@mail.ru>
2024-10-24 10:50:12 +0300

    24.10.24(5)

--- project/news/templatetags/my_filters.py
+++ project/news/templatetags/my_filters.py
@@ -5,11 +5,12 @@ register = template.Library()
 
 @register.filter()
 def correct_words(value):
-    # Используем ТАКОЙ словарь для реалистичности
-    req = requests.get('https://raw.github.com/ileygb8cwqogn8c/different_lists/master/Русский%20матерный%20словарь.txt')
-    req_add = req.text + ' ' + 'мат' # Добавим одно слово в полученную строку
-    for i in req_add.split(): #Создаем список и пробегаем его циклом
-        value = re.sub(i.capitalize(), i[0].capitalize() + '*' * (len(i) - 1), value) #Поиск с заглавной буквы
-        value = re.sub(i, i[0] + '*' * (len(i) - 1), value, flags=re.IGNORECASE)  #Игнорируем регистр
-        # Догадываюсь, что как то можно было обойтись одной строкой
+    if type(value) is str: #Проверяем на принадлежность к строковому типу
+        # Используем ТАКОЙ словарь для реалистичности
+        req = requests.get('https://raw.github.com/ileygb8cwqogn8c/different_lists/master/Русский%20матерный%20словарь.txt')
+        req_add = req.text + ' ' + 'мат' # Добавим одно слово в полученную строку
+        for i in req_add.split(): #Создаем список и пробегаем его циклом
+            value = re.sub(i.capitalize(), i[0].capitalize() + '*' * (len(i) - 1), value) #Поиск с заглавной буквы
+            value = re.sub(i, i[0] + '*' * (len(i) - 1), value, flags=re.IGNORECASE)  #Игнорируем регистр
+            # Догадываюсь, что как то можно было обойтись одной строкой
     return value
